153bea15bdf84aa6e34b5fb61d07c5e906192b9d
Danil Alshanov <danila-18@mail.ru>
2025-07-10 23:31:46 +1000

    upload-file

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,10 @@
+import { UploadFile } from "@/components/upload-file";
+
 export default function Home() {
 	return (
 		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-			<form
-				action="http://localhost:4000/upload"
-				method="POST"
-				encType="multipart/form-data"
-			>
-				<input type="file" name="myFile" />
-				<button type="submit">Upload</button>
-			</form>
+			<h1> Main page</h1>
+			<UploadFile />
 		</section>
 	);
 }
