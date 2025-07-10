842cc8b600c00d9b71715e82ac815bc9820110fe
Danil Alshanov <danila-18@mail.ru>
2025-07-10 23:49:48 +1000

    make a child client component

--- components/upload-file.tsx
+++ components/upload-file.tsx
@@ -1,36 +1,12 @@
-import axios, { AxiosHeaders } from "axios";
-import { useId, useState } from "react";
+import { UploadForm } from "./upload-form";
 
 export const UploadFile = () => {
 	const token = process.env.TOKEN;
-	const form = new FormData();
-	// const [file, setFile] = useState();
-	const upload = async () => {
-		await axios({
-			method: "post",
-			url: "https://api.openai.com/v1/audio/translations",
-			data: form,
-			headers: {
-				Authorization: `Bearer ${token}`,
-				"Content-Type": "multipart/form-data",
-			},
-		});
-	};
-	const appendFile = (event: React.ChangeEvent<HTMLInputElement>) => {
-		console.log(event.target.files, "event.target.files");
-		form.append("file", event.target.files![0]);
-		form.append("model", "whisper-1");
-	};
 
 	return (
 		<div>
 			{token}
-			<form action="">
-				<input type="file" onChange={(e) => appendFile(e)} id={"some-id"} />
-			</form>
-			<button type="button" onClick={() => upload()}>
-				upload
-			</button>
+			<UploadForm token={token} />
 		</div>
 	);
 };
---
+++ components/upload-form.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import axios from "axios";
+
+export const UploadForm = ({ token }: { token?: string }) => {
+	const form = new FormData();
+	const upload = async () => {
+		await axios({
+			method: "post",
+			url: "https://api.openai.com/v1/audio/translations",
+			data: form,
+			headers: {
+				Authorization: `Bearer ${token}`,
+				"Content-Type": "multipart/form-data",
+			},
+		});
+	};
+	const appendFile = (event: React.ChangeEvent<HTMLInputElement>) => {
+		console.log(event.target.files, "event.target.files");
+		form.append("file", event.target.files![0]);
+		form.append("model", "whisper-1");
+	};
+	return (
+		<>
+			<form action="">
+				<input type="file" onChange={(e) => appendFile(e)} id={"some-id"} />
+			</form>
+			<button type="button" onClick={() => upload()}>
+				upload
+			</button>
+		</>
+	);
+};
