340dd12ced55f0d4bca18c391ef847df0ff396bb
Danil Alshanov <danila-18@mail.ru>
2025-07-10 23:08:15 +1000

    token

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
-"use client";
-import { ProcessFile } from "@/app/api/send-file/route";
-
 export default function Home() {
+	const token = process.env.TOKEN;
 	return (
 		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-			<form action={ProcessFile} method="POST" encType="multipart/form-data">
+			{token}
+			<form
+				action="http://localhost:4000/upload"
+				method="POST"
+				encType="multipart/form-data"
+			>
 				<input type="file" name="myFile" />
 				<button type="submit">Upload</button>
 			</form>
