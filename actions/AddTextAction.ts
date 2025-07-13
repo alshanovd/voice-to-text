57de2c6634d9959fde1fc74d2b3a5d3f3f4ae5f0
Danil Alshanov <danila-18@mail.ru>
2025-07-13 20:55:39 +1000

    generated

--- actions/AddTextAction.ts
+++ actions/AddTextAction.ts
@@ -1,13 +1,13 @@
 "use server";
 
-import { PrismaClient } from "@/prisma/generated";
+import { PrismaClient } from "../prisma/generated";
 
 const prisma = new PrismaClient();
 export async function createText(formData: FormData) {
     const data = {
-        filename: "filename",
-        datetime: new Date().toISOString(),
-        fullText: "text text",
+        filename: formData.get("filename") as string,
+        datetime: new Date().toISOString() as string,
+        fullText: formData.get("fullText") as string,
     };
     const response = await prisma.text.create({ data });
     // if (data.id) {
