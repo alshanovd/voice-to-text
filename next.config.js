47521e2ee8995ae27597dca2bcab735b3c202098
Danil Alshanov <danila-18@mail.ru>
2025-07-14 00:12:00 +1000

    next js config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    optimizePackageImports: [
+        '@prisma/client',
+    ]
+};
 
 module.exports = nextConfig;
